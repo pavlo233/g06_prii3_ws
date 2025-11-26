462366e922fc0fcd2a4b3d913d0c0e4bfe2feac1
pavlo233 <pabrodbal@gmail.com>
2025-11-26 05:40:09 +0100

    launch aruco

--- src/g06_prii3_nav_turtlebot/launch/cositas_aruco_launch.py
+++ src/g06_prii3_nav_turtlebot/launch/cositas_aruco_launch.py
@@ -3,28 +3,33 @@ from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 import os
-
+from pathlib import Path 
 def generate_launch_description():
 
-    pkg_launch = os.path.join(os.getenv('HOME'), 'temp', 'g06_prii3_ws', 'src', 'g06_prii3_nav_turtlebot', 'launch')
-    map_file = os.path.join(os.getenv('HOME'), 'mapaclase.yaml')
+    pkg_dir = str(Path(__file__).parent)
+
+    # --- Rutas Locales (Modificadas para ser relativas a pkg_dir) ---
+    # f1l3_world_launch.py está en la misma carpeta
+    gazebo_launch_file = os.path.join(pkg_dir, 'f1l3_world_launch.py')
+    # mapaclase.yaml está en la misma carpeta
+    map_file = os.path.join(pkg_dir, 'mapaclase.yaml')
     nav2_launch_file = '/opt/ros/foxy/share/turtlebot3_navigation2/launch/navigation2.launch.py'
     rviz_file = '/opt/ros/foxy/share/turtlebot3_navigation2/rviz/nav2_default_view.rviz'
 
     return LaunchDescription([
 
-        # Tu mundo propio en Gazebo
+        # Tu mundo propio en Gazebo (Usa la ruta local)
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(os.path.join(pkg_launch, 'f1l3_world_launch.py'))
+            PythonLaunchDescriptionSource(gazebo_launch_file)
         ),
 
-        # Nav2 oficial SIN RViz
+        # Nav2 oficial SIN RViz (Usa la ruta absoluta)
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(nav2_launch_file),
             launch_arguments={
                 'use_sim_time': 'True',
-                'map': map_file,
-                'use_rviz': 'False'  # Evita que Nav2 abra su propio RViz
+                'map': map_file,  # El mapa sí usa la ruta local
+                'use_rviz': 'False'
             }.items()
         ),
 
